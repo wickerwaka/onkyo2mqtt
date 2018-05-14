e3cc7c549c51b166cd44321857dd7531caeb8b2a
Jeff Licquia <jeff@licquia.org>
2018-05-13 20:32:41 -0400

    Un-tab.

--- onkyo2mqtt.py
+++ onkyo2mqtt.py
@@ -38,7 +38,7 @@ if args.syslog:
 
 topic=args.mqtt_topic
 if not topic.endswith("/"):
-	topic+="/"
+        topic+="/"
 
 lastSend=0
 
@@ -54,18 +54,18 @@ def sendavr(cmd):
     logging.info("Sent command %s" % (cmd))
 
 def msghandler(mqc,userdata,msg):
-	try:
-		global topic
-		if msg.retain:
-			return
-		mytopic=msg.topic[len(topic):]
-		if mytopic=="command":
-			sendavr(msg.payload)
-		elif mytopic[0:4]=="set/":
-			llcmd=eiscp.core.command_to_iscp(mytopic[4:]+" "+msg.payload)
-			sendavr(llcmd)
-	except Exception as e:
-		logging.warning("Error processing message %s" % e)
+        try:
+                global topic
+                if msg.retain:
+                        return
+                mytopic=msg.topic[len(topic):]
+                if mytopic=="command":
+                        sendavr(msg.payload)
+                elif mytopic[0:4]=="set/":
+                        llcmd=eiscp.core.command_to_iscp(mytopic[4:]+" "+msg.payload)
+                        sendavr(llcmd)
+        except Exception as e:
+                logging.warning("Error processing message %s" % e)
 
 def connecthandler(mqc,userdata,rc):
     logging.info("Connected to MQTT broker with rc=%d" % (rc))
@@ -86,54 +86,53 @@ mqc.connect(args.mqtt_host,args.mqtt_port,60)
 mqc.publish(topic+"connected",1,qos=1,retain=True)
 
 if args.onkyo_address:
-	receiver=eiscp.eISCP(args.onkyo_address)
+        receiver=eiscp.eISCP(args.onkyo_address)
 else:
-	logging.info('Starting auto-discovery of Onkyo AVRs')
-	receivers=eiscp.eISCP.discover()
-	for receiver in receivers:
-		logging.info("Disocvered %s at %s:%s with id %s" % (
-			receiver.info['model_name'], receiver.host, receiver.port, receiver.info['identifier']))
-	if args.onkyo_id:
-		receivers=[r for r in receivers
-			if args.onkyo_id in r.info['identifier']]
-	if len(receivers)==0:
-		logging.warning("No specified AVRs discovered")
-		exit(1)
-	elif len(receivers)!=1:
-		logging.warning("More than one AVR discovered, please specify explicitely using --onkyo-address or --onkyo-id")
-		exit(1)
-	receiver=receivers.pop(0)
-	logging.info('Discovered AVR at %s',receiver)
+        logging.info('Starting auto-discovery of Onkyo AVRs')
+        receivers=eiscp.eISCP.discover()
+        for receiver in receivers:
+                logging.info("Disocvered %s at %s:%s with id %s" % (
+                        receiver.info['model_name'], receiver.host, receiver.port, receiver.info['identifier']))
+        if args.onkyo_id:
+                receivers=[r for r in receivers
+                        if args.onkyo_id in r.info['identifier']]
+        if len(receivers)==0:
+                logging.warning("No specified AVRs discovered")
+                exit(1)
+        elif len(receivers)!=1:
+                logging.warning("More than one AVR discovered, please specify explicitely using --onkyo-address or --onkyo-id")
+                exit(1)
+        receiver=receivers.pop(0)
+        logging.info('Discovered AVR at %s',receiver)
 
 # Query some initial values 
 for icmd in ("PWR","MVL","SLI","SLA","LMD"):
-	sendavr(icmd+"QSTN")
+        sendavr(icmd+"QSTN")
 
 mqc.loop_start()
 
 def publish(suffix,val,raw):
-	global topic,mqc
-	robj={}
-	robj["val"]=val
-	if raw is not None:
-	    robj["onkyo_raw"]=raw
-	mqc.publish(topic+"status/"+suffix,json.dumps(robj),qos=0,retain=True)
+        global topic,mqc
+        robj={}
+        robj["val"]=val
+        if raw is not None:
+            robj["onkyo_raw"]=raw
+        mqc.publish(topic+"status/"+suffix,json.dumps(robj),qos=0,retain=True)
 
 while True:
-	msg=receiver.get(3600)
-	if msg is not None:
-		try:
-			parsed=eiscp.core.iscp_to_command(msg)
-			# Either part of the parsed command can be a list
-			if isinstance(parsed[1],str) or isinstance(parsed[1],int):
-				val=parsed[1]
-			else:
-				val=parsed[1][0]
-			if isinstance(parsed[0],str):
-				publish(parsed[0],val,msg)
-			else:
-				for pp in parsed[0]:
-					publish(pp,val,msg)
-		except:
-			publish(msg[:3],msg[3:],msg)
-			
+        msg=receiver.get(3600)
+        if msg is not None:
+                try:
+                        parsed=eiscp.core.iscp_to_command(msg)
+                        # Either part of the parsed command can be a list
+                        if isinstance(parsed[1],str) or isinstance(parsed[1],int):
+                                val=parsed[1]
+                        else:
+                                val=parsed[1][0]
+                        if isinstance(parsed[0],str):
+                                publish(parsed[0],val,msg)
+                        else:
+                                for pp in parsed[0]:
+                                        publish(pp,val,msg)
+                except:
+                        publish(msg[:3],msg[3:],msg)
